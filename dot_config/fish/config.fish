60b2c21e849a335a342d42267c56947a9ec5d26b
Fung <sflam6@gmail.com>
2025-05-12 23:10:11 +0800

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,2 +1,11 @@
 source ~/.bashrc
 export EDITOR=/usr/bin/nvim
+set -gx EDITOR nvim
+
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
